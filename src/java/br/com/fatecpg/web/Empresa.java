8e5a1c0e6c4f99f7f56ee8659b142e4ba0d7d8ab
EduardoSimao <edu-simao@outlook.com>
2018-10-13 13:25:21 -0300

    Classe Empresa com os métodos getters e setters dos atributos

--- src/java/br/com/fatecpg/web/Empresa.java
+++ src/java/br/com/fatecpg/web/Empresa.java
@@ -10,5 +10,57 @@ package br.com.fatecpg.web;
  * @author Positivo
  */
 public class Empresa {
+    private String nome;
+    private String razaoSocial;
+    private String cnpj;
+    private String telefone;
+    private String website;
     
+    public void setDados(String nome, String razaoSocial, String cnpj, String telefone, String website){
+        this.setNome(nome);
+        this.setRazaoSocial(razaoSocial);
+        this.setCnpj(cnpj);
+        this.setTelefone(telefone);
+        this.setWebsite(website);
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
 }
